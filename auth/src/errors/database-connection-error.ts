a0bd65908defe1cf6766eb56eed215cddb15e4db
Matheus Lino <matheuslino86a@gmail.com>
2022-12-22 14:57:43 -0400

    Signup error classes

---
+++ auth/src/errors/database-connection-error.ts
@@ -0,0 +1,9 @@
+export class DatabaseConnectionError extends Error {
+    reason = "Error connecting to database";
+    constructor() {
+        super();
+
+        // Only because we are extending a built in Typescript class
+        Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+    }
+}
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 const router = express.Router();
 
@@ -16,16 +18,11 @@ router.post(
         const errors = validationResult(req);
 
         if (!errors.isEmpty()) {
-            return res.status(400).send(errors.array());
+            throw new RequestValidationError(errors.array());
         }
-
-        const { email, password } = req.body;
-
         console.log("Creating a user...");
+        throw new DatabaseConnectionError();
 
-        if (!email || typeof email !== "string") {
-            res.status(400).send("Provide a valid email");
-        }
         res.send({});
     }
 );
