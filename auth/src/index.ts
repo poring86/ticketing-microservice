0909845759440607cdeff9e3e44f1ad8dd22ee2f
Matheus Lino <matheuslino86a@gmail.com>
2022-12-23 17:12:09 -0400

    Set JWT secret config

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -9,8 +9,16 @@ import { signupRouter } from "./routes/signup";
 import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundError } from "./errors/not-found-error";
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 
 const app = express();
+app.set("trust proxy", true);
+app.use(
+    cookieSession({
+        signed: false,
+        secure: true,
+    })
+);
 app.use(json());
 
 app.use(currentUserRouter);
@@ -25,6 +33,10 @@ app.all("*", async (req, res, next) => {
 app.use(errorHandler);
 
 const start = async () => {
+    if (!process.env.JWT_KEY) {
+        throw new Error("JWT_KEY must be defined");
+    }
+
     try {
         await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
         console.log("Connected to mongodb");
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import jwt from "jsonwebtoken";
+
 import { RequestValidationError } from "../errors/request-validation-error";
 import { User } from "../models/user";
 import { BadRequestError } from "../errors/bad-request-error";
@@ -32,6 +34,18 @@ router.post(
 
         const user = User.build({ email, password });
         await user.save();
+
+        // Generate jwt
+        const userJwt = jwt.sign(
+            {
+                id: user.id,
+                email: user.email,
+            },
+            process.env.JWT_KEY!
+        );
+
+        // Store on session
+        req.session = { jwt: userJwt };
 
         res.status(201).send(user);
     }
