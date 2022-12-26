c4fc9c726e62c42f4ed769ba9e2fbdd53fdd4b31
Matheus Lino <matheuslino86a@gmail.com>
2022-12-26 09:37:59 -0400

    Test coockie after sinup

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -51,3 +51,15 @@ it("disallows duplicate emails", async () => {
         })
         .expect(400);
 });
+
+it("sets a cookie after a successful sinup", async () => {
+    const response = await request(app)
+        .post("/api/users/signup")
+        .send({
+            email: "test@test.com",
+            password: "password",
+        })
+        .expect(201);
+
+    expect(response.get("Set-Cookie")).toBeDefined();
+});
