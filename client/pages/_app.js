ae9e33d7de4ab4cc3eac40c08e6481ba0496aa05
Matheus Lino <matheuslino86a@gmail.com>
2022-12-27 15:20:28 -0400

    Set header

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,32 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import buildClient from "../api/build-client";
 
-export default ({ Component, pageProps }) => {
-    return <Component {...pageProps} />;
+import Header from "../components/header";
+
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+    return (
+        <div>
+            <Header currentUser={currentUser} />
+            <Component {...pageProps} />
+        </div>
+    );
 };
+
+AppComponent.getInitialProps = async (appContext) => {
+    const client = buildClient(appContext.ctx);
+
+    const { data } = await client.get("/api/users/currentuser");
+
+    let pageProps = {};
+
+    if (appContext.Component.getInitialProps) {
+        pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+    }
+
+    return {
+        pageProps,
+        ...data,
+    };
+};
+
+export default AppComponent;
