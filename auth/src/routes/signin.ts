c4f12a0158b69007b828ac1536bc02fd4cbc3e44
Matheus Lino <matheuslino86a@gmail.com>
2022-12-27 18:41:24 -0400

    Common added

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -2,9 +2,8 @@ import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import jwt from "jsonwebtoken";
 
-import { validateRequest } from "../middlewares/validate-request";
+import { validateRequest, BadRequestError } from "@mattlino/common";
 import { User } from "../models/user";
-import { BadRequestError } from "../errors/bad-request-error";
 import { Password } from "../services/password";
 
 const router = express.Router();
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -3,8 +3,7 @@ import { body } from "express-validator";
 import jwt from "jsonwebtoken";
 
 import { User } from "../models/user";
-import { BadRequestError } from "../errors/bad-request-error";
-import { validateRequest } from "../middlewares/validate-request";
+import { BadRequestError, validateRequest } from "@mattlino/common";
 
 const router = express.Router();
 
