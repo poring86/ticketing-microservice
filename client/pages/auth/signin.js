e375456dc749b8cf2cc0f227cceea90f72c1376d
Matheus Lino <matheuslino86a@gmail.com>
2022-12-27 13:14:23 -0400

    Signin form

---
+++ client/pages/auth/signin.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import useRequest from "../../hooks/use-request";
+
+import Router from "next/router";
+
+export default () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const { doRequest, errors } = useRequest({
+        url: "/api/users/signin",
+        method: "post",
+        body: {
+            email,
+            password,
+        },
+        onSuccess: () => Router.push("/"),
+    });
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+
+        await doRequest();
+    };
+
+    return (
+        <form onSubmit={onSubmit}>
+            <h1>Sign In</h1>
+            <div className="form-group">
+                <label>Email address</label>
+                <input
+                    className="form-control"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+            </div>
+            <div className="form-group">
+                <label>Password</label>
+                <input
+                    type="password"
+                    className="form-control"
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            </div>
+            {errors}
+
+            <button className="btn btn-primary">Sign In</button>
+        </form>
+    );
+};
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,11 @@
 import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
-    return <h1>Landing page</h1>;
+    return currentUser ? (
+        <h1>You are signed in</h1>
+    ) : (
+        <h1>You are NOT signed in</h1>
+    );
 };
 
 LandingPage.getInitialProps = async (context) => {
